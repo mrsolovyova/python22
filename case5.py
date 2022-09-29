0442b6207e50479ba1de7e298aa97ff0b735fef2
Maria Solovyova <mary.solovyova@gmail.com>
2022-09-29 14:00:09 +0300

    Обновление файла с учетом комментариев

--- case5.py
+++ case5.py
@@ -1,16 +1,17 @@
+PHONE_BOOK = {"Иван": "3916", "Катя": "1298",
+              "Лена": "0010", "Иосиф": "1953",
+              "Владимир": "2022"}
+
+
 # функция поиска имени в справочнике и выдачи номера телефона при соответствии
 def search():
-    for key in phone_book:
+    for key in PHONE_BOOK:
         if check_name == key:
-            print(phone_book[key])
+            print(PHONE_BOOK[key])
             break
     else:
         print("undefined")
 
-
-phone_book = {"Иван": "3916", "Катя": "1298",
-              "Лена": "0010", "Иосиф": "1953",
-              "Владимир": "2022"}
 
 check_name = input("Введите имя: ")
 search()
