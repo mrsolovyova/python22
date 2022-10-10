74ab59172a68144c17b1a1734f04ff440d3a96ad
Maria Solovyova <mary.solovyova@gmail.com>
2022-10-10 21:14:32 +0300

    внесла поправки

--- case_6/create_database.py
+++ case_6/create_database.py
@@ -1,5 +1,4 @@
 import sqlite3
-import pandas as pd
 
 connection = sqlite3.connect("cities.db")
 
@@ -17,9 +16,10 @@ cursor.execute(create_table_region)
 create_table_city = """
     CREATE TABLE city(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        city_name TEXT NOT NULL UNIQUE,
-        region_id INTEGER NOT NULL UNIQUE,
+        city_name TEXT NOT NULL,
+        region_id INTEGER NOT NULL,
         FOREIGN KEY(region_id) REFERENCES region(id)
+        CONSTRAINT UC_city UNIQUE (city_name, region_id)
         );
 """
 cursor.execute(create_table_city)
