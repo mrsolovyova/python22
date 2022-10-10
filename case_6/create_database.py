85f989a549ced8bfc68258cd136d9dbd6f34c998
Maria Solovyova <mary.solovyova@gmail.com>
2022-10-10 20:53:34 +0300

    cases for database

--- case_6/create_database.py
+++ case_6/create_database.py
@@ -9,7 +9,7 @@ with sqlite3.connect("cities.db") as connection:
 create_table_region = """
     CREATE TABLE region(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        region_name TEXT NOT NULL
+        region_name TEXT NOT NULL 
     );
 """
 cursor.execute(create_table_region)
@@ -17,8 +17,8 @@ cursor.execute(create_table_region)
 create_table_city = """
     CREATE TABLE city(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        city_name TEXT NOT NULL,
-        region_id INTEGER NOT NULL,
+        city_name TEXT NOT NULL UNIQUE,
+        region_id INTEGER NOT NULL UNIQUE,
         FOREIGN KEY(region_id) REFERENCES region(id)
         );
 """
